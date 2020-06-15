2e44c9246bf48cb59a12b3fd44371ec07b1a21b1
Elan Qisthi <elan.qisthi@bukalapak.com>
2020-06-15 18:17:38 +0700

    delete debug code

--- app/main.go
+++ app/main.go
@@ -16,9 +16,4 @@ func main() {
 
 	fmt.Println("Bot is now running")
 	ninu.TelegramBot.Start()
-	url := "https://twitter.com/LaLigaEN/status/1272107009232396289"
-	err := ninu.ProcessURL(url, "thomas_partey.mp4")
-	if err != nil {
-		panic(err)
-	}
 }
